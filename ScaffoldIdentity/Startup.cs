8a0d90b6c3a2fc6fa87a9011b015da738531d965
Alain Nshimirimana <alain.nshimirimana@gmail.com>
2020-05-18 12:51:20 -0700

    Able to get to the register and login views

--- ScaffoldIdentity/Startup.cs
+++ ScaffoldIdentity/Startup.cs
@@ -24,6 +24,8 @@ namespace ScaffoldIdentity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,12 +47,14 @@ namespace ScaffoldIdentity
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
